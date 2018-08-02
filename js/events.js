3d36c3bef5ed1adec2d5f839120e7837c244a6cd
Ramon Posadas Martinez <posadas_r@hotmail.com>
2018-08-01 21:20:39 -0500

    WIP

--- js/events.js
+++ js/events.js
@@ -15,3 +15,45 @@ function domSlider(idSlider) {
 }
 
 domSlider('Slider');
+
+
+
+const formNode = document.getElementById('register-form')
+
+formNode.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const nameNode = document.getElementById('name');
+    const mailNode = document.getElementById('mail');
+    const passwordNode = document.getElementById('password');
+    let data = {
+        name: nameNode.value,
+        mail: mailNode.value,
+        password: passwordNode.value,
+    };
+
+    const myRequest = new Request(
+        'http://localhost:3000/users',
+        {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+    );
+
+    fetch(myRequest)
+    .then((response) => {
+        if (response.status === 201) {
+            return response.json();
+        } else {
+            throw new Error('Something went wrong on api server!');
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    }).catch(error => {
+        console.error(error);
+    })
+});
